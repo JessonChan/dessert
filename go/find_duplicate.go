ac59cd77368706ed7a73fe7e2cc2a39424b461fb
JessonChan <chinachendongpo@gmail.com>
2012-09-28 23:20:26 +0800

    fix逻辑硬伤
    当ia[0,1]的情况

--- go/find_duplicate.go
+++ go/find_duplicate.go
@@ -1,8 +1,3 @@
-/*
-http://weibo.com/1915548291/yDjra5ajO#1348465113389
-一个大小为n的数组，里面的数都属于范围[0, n-1]，有不确定的重复元素，
-找到至少一个重复元素，要求O(1)空间和O(n)时间。
-*/
 package main
 
 import "fmt"
@@ -12,14 +7,20 @@ func find_duplicate(ia []int) bool {
 		if ia[i] < 0 {
 			return true
 		}
-		t := ia[i]
-		ia[i] = -1
-		i = t
+		if ia[i] == i {
+			ia[i] = -1
+			i++
+		} else {
+			t := ia[i]
+			ia[i] = -1
+			i = t
+		}
 	}
 	return false
 }
 
 func main() {
 	ia := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 8}
+	//	ia :=[]int{0,1}
 	fmt.Println(find_duplicate(ia))
 }
